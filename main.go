ee45cc088115a00b3a55af36c8f1410a6308f209
ShuangWu121 <shuang.wu@posteo.net>
2024-08-22 14:42:58 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/consensys/gnark-crypto/ecc"
+	"github.com/consensys/gnark/frontend"
+
+	"github.com/PolyhedraZK/ExpanderCompilerCollection"
+	"github.com/PolyhedraZK/ExpanderCompilerCollection/test"
+)
+
+type Circuit struct {
+	X      frontend.Variable `gnark:",public"`
+	Y      frontend.Variable `gnark:",public"`
+	Result frontend.Variable `gnark:",public"`
+}
+
+func (circuit *Circuit) Define(api frontend.API) error {
+	var rounds int = 2
+
+	var FibonacciResult frontend.Variable = api.Add(circuit.X, circuit.Y)
+
+	for i := 0; i < rounds; i++ {
+		fmt.Println("round")
+		circuit.X = circuit.Y
+		circuit.Y = FibonacciResult
+		FibonacciResult = api.Add(circuit.Y, circuit.X)
+	}
+	api.AssertIsEqual(circuit.Result, FibonacciResult)
+	return nil
+}
+
+func main() {
+	assignment := &Circuit{X: 1, Y: 1, Result: 5}
+
+	circuit, _ := ExpanderCompilerCollection.Compile(ecc.BN254.ScalarField(), &Circuit{})
+	//fmt.Println("circuit is ", circuit)
+	c := circuit.GetLayeredCircuit()
+	//fmt.Println("layered circuit is ", c)
+
+	data, err := json.MarshalIndent(c, "", "  ")
+	if err != nil {
+		log.Fatalf("Failed to serialize data: %v", err)
+	}
+
+	err = os.WriteFile("circuitjson.txt", data, 0o644)
+	if err != nil {
+		log.Fatalf("Failed to write file: %v", err)
+	}
+
+	os.WriteFile("circuit.txt", c.Serialize(), 0o644)
+	inputSolver := circuit.GetInputSolver()
+	witness, _ := inputSolver.SolveInputAuto(assignment)
+
+	witnessdata, err := json.MarshalIndent(witness, "", "  ")
+	if err != nil {
+		log.Fatalf("Failed to serialize data: %v", err)
+	}
+
+	err = os.WriteFile("witnessjson.txt", witnessdata, 0o644)
+	if err != nil {
+		log.Fatalf("Failed to write file: %v", err)
+	}
+
+	os.WriteFile("witness.txt", witness.Serialize(), 0o644)
+
+	if !test.CheckCircuit(c, witness) {
+		panic("verification failed")
+	}
+
+}
